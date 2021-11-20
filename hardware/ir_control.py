4905efb9c0a3de94f37eb9f2988d7adde35a08ce
Francois-Bergeron <frankyboy_89@hotmail.com>
2021-11-20 15:20:50 -0500

    Working IR Beam code. Need to check if the threaded listener can send to main thread

--- hardware/ir_control.py
+++ hardware/ir_control.py
@@ -1,82 +1,71 @@
 import RPi.GPIO as GPIO  # pip install RPi.GPIO
-import lirc # pip install lirc - LIRC should be install on system (sudo apt-get install lirc)
-"""
-1.add to LIRC GPIO connection:
-`sudo nano /etc/modules`
-Add these 2 lines:
-`lirc_dev`
-`lirc_rpi gpio_in_pin=18 gpio_out_pin=17` Make sure you match to LED/RECEIVER PINS
-2.Then modify:
-`sudo nano /etc/lirc/hardware.conf`
-Add these 3 lines:
-`DRIVER="default"`
-`DEVICE="/dev/lirc0"`
-`MODULES="lirc_rpi"`
-3.Reboot your Pi.
-4.Add a remote/key combo. (This will be hardcoded once done. TODO!)
-"""
+import sys
 import time
 import threading
 
 # TODO: Test Infrared script to confirm activation / stop
 
 
-
 class IrLed:
     """
-    Enabling control of the infrared LED at 38kHz
+    Enabling control of the infrared LED beam sensor
     """
-
-    def __init__(self, debug=False, lirc_remote='Remote', lirc_key='Key'):
+    def __init__(self, debug=False):
         self.debug = bool(debug)
         self.ir_led_pin = 11 # Physical location (GPIO pin# 17)
         self.ir_sensor_pin = 12 # Physical location (GPIO pin# 18)
-        self.lirc_client = lirc.Client()
-        self.lirc_remote = lirc_remote
-        self.lirc_key = lirc_key 
         self.state = False  # Beam connected = True, Broken =  False
         self.max_state_trigger = 5  # After X refresh rate (default 10Hz, 5step -> 500ms), confirms the mouse is in place
         self.irb_broken_counter = 0
         self.irb_complete_counter = 0
-        self.is_irb_broken = False  # Beam disrupted trigger (mouse is there)
-        if self.debug:
-            print(self.client.version())
-            print(self.lirc_remote)
-            print(self.lirc_key)
+        self.is_irb_broken = False  # Beam disrupted trigger (mouse is there)      
 
     def setup(self):
         # Setup Receiver
         GPIO.setmode(GPIO.BOARD)    # Numbers GPIOs by physical location
         # GPIO.setmode(GPIO.BCM)    # Numbers GPIOs by GPIO
+        GPIO.setup(self.ir_led_pin, GPIO.OUT)
         GPIO.setup(self.ir_sensor_pin, GPIO.IN)
-        # Start sending IR
-        client = lirc.Client()
-        client.send_start(self.lirc_remote, self.lirc_key)
         # Update state
         self.is_irb_broken = False
         self.state = False
-        
-    
+
     def check_beam_status(self):
-        while not(self.is_irb_broken):
-            time.sleep(0.100) # 10Hz refresh rate
+        timer = 0
+        debug_timer = 11 # 10 sec debug session
+        step = 0.100 # 10Hz refresh rate
+        while timer < debug_timer:
+            time.sleep(step) # 10Hz refresh rate
+            # import pdb; pdb.set_trace()
             self.state = bool(GPIO.input(self.ir_sensor_pin))
             if self.state:  # If beam is complete
                 self.irb_broken_counter = 0
                 self.irb_complete_counter += 1
-            if self.irb_complete_counter >= self.max_state_trigger:
-                self.is_irb_broken = False
+                if self.irb_complete_counter >= self.max_state_trigger:
+                    self.is_irb_broken = False
             else:  # If beam is broken
                 self.irb_complete_counter = 0
                 self.irb_broken_counter += 1
-            if self.irb_broken_counter >= self.max_state_trigger:
-                self.is_irb_broken = True
+                if self.irb_broken_counter >= self.max_state_trigger:
+                    self.is_irb_broken = True
+            # While debugging print beam status every 500 ms
+            if self.debug:
+                timer += step
+                print('Is Beam Broken: ', self.is_irb_broken)
         return self.is_irb_broken
 
+    def start_irb(self):
+        self.start_ir()
+        # Start the listener
+        self.listener_beam()
+
+    def start_ir(self):
+        GPIO.output(self.ir_led_pin,GPIO.HIGH)
+        return bool(GPIO.input(self.ir_led_pin))
 
     def stop_ir(self):
-        client.send_stop(self.lirc_remote, self.lirc_key)
-        GPIO.cleanup()
+        GPIO.output(self.ir_led_pin,GPIO.LOW)
+        return bool(GPIO.input(self.ir_led_pin))
 
     def listener_beam(self):
         # Listener to update beam. Never stop until experiments stops (daemon=true)
@@ -84,12 +73,19 @@ class IrLed:
         thread.start()
 
 
-
-client.send_stop()
-
-"""
-client.simulate()
-Simulate an IR event.
-
-The --allow-simulate command line option to lircd must be active for this command not to fail.
-"""
+###################################
+## TEST WHEN CALLING THIS SCRIPT ##
+###################################
+if __name__ == '__main__':
+    debug=True
+    if debug:
+        irb = IrLed(debug=debug)
+        irb.setup()
+        print('Try to break the beam. Status is printed every 500ms.')
+        print('Test will last 10 sec')
+        # import pdb; pdb.set_trace()
+        time.sleep(1)
+        irb.start_ir()
+        irb.check_beam_status()
+        print('End of test.')
+        GPIO.cleanup()
