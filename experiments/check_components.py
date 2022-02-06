41576bb64ac2a47513eb597fe9c04f0a64242f73
oliviabharvey <olivia.bharvey@gmail.com>
2022-02-06 13:10:00 -0500

    [experiment] added experiment that checks all components

--- experiments/check_components.py
+++ experiments/check_components.py
@@ -3,6 +3,8 @@ import random
 
 from experiments.base_exp import BaseExperiment
 from utils.enums import *
+from hardware.motor_control import MotorControl
+from hardware.ir_control import IrLed
 
 class CheckComponents(BaseExperiment):
     """
@@ -17,8 +19,32 @@ class CheckComponents(BaseExperiment):
         """
         Starting experiment with first steps.
         """
-        super().initialize()
-        self.initialize_touch_screen_helper(DisplayPatterns.NONE)
+        sys.stdout.write("\nChecking all components")
+        sys.stdout.write("\nInitializing tests and turning experiment light on")
+        super().initialize() # calls the initalization method within the superclass BaseExperiment
+        time.sleep(2)
+        # Check buzzer
+        sys.stdout.write('\nChecking buzzer')
+        self.hardware_connector.play_tone(duration=2)
+        time.sleep(2)
+        # Check light of the tray
+        sys.stdout.write('\nChecking tray light')
+        self.hardware_connector.turn_tray_light_on()
+        time.sleep(2)
+        # Check motor
+        sys.stdout.write('\nChecking motor')
+        motor = MotorControl()
+        motor.setup()
+        sys.stdout.write('\nTurning clockwise full turn')
+        motor.full_rotation(2)
+        time.sleep(1)
+        sys.stdout.write('\nTurning counterclockwise full turn')
+        motor.full_rotation(-2)
+        time.sleep(1)
+        # Check IR
+        sys.stdout.write("\nChecking IR")
+        irled = IrLed()
+        irled.check_ir()
 
     def update_state(self):
         return
